8bb69464bc5c922da37ea5be9f44dc9a2d498d4f
Amandine CERDAN <acerdan@e1r10p7.clusters.42paris.fr>
2022-10-10 08:50:18 +0200

    if channel exist do not make else cause he dont't come in this condition

--- srcs/Server/ServerCommands/ServerCommandJoin.cpp
+++ srcs/Server/ServerCommands/ServerCommandJoin.cpp
@@ -15,12 +15,13 @@ void Server::_join(std::string args, User &user)
 	(void)args;
 	(void)user;
 
-/*
+
 	std::string rowChannels;
 	std::vector<std::string> channels;
 	std::vector<Channel>::iterator channelIt;
 
 	rowChannels = Utils::split(args, ' ')[0];
+	std::cout << "------> rowChannels = " <<  rowChannels << std::endl;
 	channels = Utils::split(rowChannels, ',');
 
 	Channel c;
@@ -35,12 +36,13 @@ void Server::_join(std::string args, User &user)
 		if (!hasChannel(*itb))
 		{
 			//create channel
-			std::cout << "Create New Channel" << std::endl;
+			std::cout << "Create New Channel : " << *itb << std::endl;
 			_channels.push_back(Channel(*itb));
 			channelIt = findChannel(*itb);
 			user.addChannel(channelIt);
 		}
-		else
+		/* ne prends plus en compte, /join toto, si toto deja creer, y va sans passer par resverProcess
+ 		else
 		{
 			//welcome on this channel
 			channelIt = findChannel(*itb);
@@ -48,12 +50,12 @@ void Server::_join(std::string args, User &user)
 			_commandResponces(user, RPL_TOPIC, "JOIN", *channelIt);
 			std::cout << user.nickName << " joins " << channelIt->_channelName << std::endl;
 			nameReply(user, *channelIt);
-		}
+		}*/
 	}
 
 	//A JOIN message with the client as the message <source> and the channel they have joined as the first parameter of the message.
 	// RPL_TOPIC (332)
 	// one or more RPL_NAMREPLY (353) + RPL_ENDOFNAMES (366)
 	//"<client> <symbol> <channel> :[prefix]<nick>{ [prefix]<nick>}"
-	*/
+	
 }
